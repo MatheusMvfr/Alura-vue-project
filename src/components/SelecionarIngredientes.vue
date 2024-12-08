f6c2e17767a9586b47afbd70c38f9a56613e0841
Matheus Vitor Ferreira Ramos <matheusvitorferreiraramos@MacBook-Air-de-Matheus.local>
2024-12-08 15:12:30 -0300

    feat(front): Incluindo rodapé e implementando a mecânica de remoção de itens da lista de ingredientes.

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -2,9 +2,11 @@
 import { obterCategorias } from "@/http/index";
 import type ICategoria from "@/interfaces/ICategoria";
 import CardCategoria from "./CardCategoria.vue";
+import BotaoPrincipal from "./BotaoPrincipal.vue";
 export default {
   components: {
     CardCategoria,
+    BotaoPrincipal,
   },
   emits: ["adicionarIngrediente", "removerIngrediente"],
   data() {
@@ -36,6 +38,7 @@ export default {
     <p class="paragrafo dica">
       *Atenção: consideramos que você tem em casa sal pimenta e água.
     </p>
+    <BotaoPrincipal texto="Buscar receitas!" />
   </section>
 </template>
 
